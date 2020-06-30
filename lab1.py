b4988928ca72cebd1c2868fde37e0c31468be25d
cinumbenu <billccie@yahoo.com>
2020-06-30 08:53:00 -0700

    initial code

---
+++ lab1.py
@@ -0,0 +1,11 @@
+a = 3
+
+def math():
+	if a == 0:
+		print('a = 0')
+	elif a == 1:
+		print('a = 1')
+	elif a > 1 and a < 4:
+		print('a is between 2 and 3')
+	else:
+		print ('a is greater than 3)
